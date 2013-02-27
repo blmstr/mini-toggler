6a1b7cc06c9dfb24730e90538cb157aabff356f8
Joel Larsson <bolmaster2@gmail.com>
2013-02-27 15:04:16 +0100

    clean up. add add/remove-class helper functions, remove string.trim-polyfill (ie8 is not supported either way because of the use of addeventlistener)

--- mini-toggler.js
+++ mini-toggler.js
@@ -1,44 +1,47 @@
 // mini toggler
 (function() {
-  // add trim method cross browser
-  if (!String.prototype.trim) { String.prototype.trim = function () {return this.replace(/^\s+|\s+$/g,'');}; }
+
+  // get all the mini-togglers
   var togglers = document.querySelectorAll(".mini-toggler");
+  
+  // loop through the togglers and bind a click event
   for (var i = 0; i < togglers.length; i++) {
     togglers[i].addEventListener("click", function(e) {
       e.preventDefault();
 
-      var hash = this.href.split('#'),
-          id = hash[hash.length-1],
-          toggle_content = document.getElementById(id),
-          group = this.getAttribute("data-toggler-group");
+      // set the vars to work with
+      var toggle_content = document.getElementById(this.hash.replace("#", "")),
+          group = this.getAttribute("data-toggler-group"),
+          add_class = function(el, classname) {
+            el.className += " "+classname;
+          },
+          remove_class = function(el, classname) {
+            el.className = el.className.replace(classname, "").trim();
+          };
 
       // if we have a data-toggler-group-attribute - toggle between them
       if (group) {
         var group_togglers = document.querySelectorAll("[data-toggler-group="+group+"]");
         for (var i = 0; i < group_togglers.length; i++) {
-          if (group_togglers[i] == this) {
-            continue;
-          }
+          // leave the current element you clicked alone
+          if (group_togglers[i] == this) { continue; }
+
           // remove toggler active class
-          group_togglers[i].className = group_togglers[i].className.replace("toggler-active", "").trim();
-          
-          // get the toggler content
-          var hash = group_togglers[i].href.split('#'),
-              id = hash[hash.length-1],
-              group_toggler_content = document.getElementById(id);
+          remove_class(group_togglers[i], "toggler-active");
           // remove toggler content active class 
-          group_toggler_content.className = group_toggler_content.className.replace("active", "").trim();
+          remove_class(document.getElementById(group_togglers[i].hash.replace("#", "")), "active");
         }
       }
 
-      // add/remove class name
+      // add/remove class name from both toggler and toggle content
       if (toggle_content.className.indexOf("active") == -1) {
-        this.className += " toggler-active";
-        toggle_content.className += " active";
+        add_class(this, "toggler-active");
+        add_class(toggle_content, "active");
       } else {
-        toggle_content.className = toggle_content.className.replace("active", "").trim();
-        this.className = this.className.replace("toggler-active", "").trim();
+        remove_class(toggle_content, "active");
+        remove_class(this, "toggler-active");
       }
     });
   }
+
 })();
